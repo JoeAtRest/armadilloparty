0ecb04187673c2d88faeb93fc2aa1b56f04d320e
Joseph Bunting <joseph.bunting@hah.co.uk>
2021-12-26 11:31:10 +0000

    added https and removed minified styles

--- Startup.cs
+++ Startup.cs
@@ -66,7 +66,7 @@ namespace SampleMvcApp
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
-
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
